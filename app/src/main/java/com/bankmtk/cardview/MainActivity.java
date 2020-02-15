5b6b427785484b8df14162e54142e1574a47f996
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-02-15 20:11:28 +0300

    MainActivity

--- app/src/main/java/com/bankmtk/cardview/MainActivity.java
+++ app/src/main/java/com/bankmtk/cardview/MainActivity.java
@@ -1,8 +1,13 @@
 package com.bankmtk.cardview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        DataSourceBuilder builder = new DataSourceBuilder(getResources());
+
+        SocnetAdapter adapter = new SocnetAdapter(builder.build());
+        recyclerView.setAdapter(adapter);
+
+        final Activity that = this;
+
+        adapter.SetOnItemClickListener(new SocnetAdapter.OnItemClicklistener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(that, String.format("Position -%d", position),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
